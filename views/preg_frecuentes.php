72928e7126137016fa724786666985ca29bb5344
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-04-29 16:14:10 -0300

    redacción de contenido view FAQs

--- views/preg_frecuentes.php
+++ views/preg_frecuentes.php
@@ -9,5 +9,54 @@
                 <h2 class="ps-3">Preguntas frecuentes</h2>
             </div>
         </div>
+        <div class="col-12">
+            <p>Para ayudarte a resolver tus dudas, hemos preparado una lista de preguntas frecuentes.</p>
+        </div>
+
+        <div class="col-6 d-flex flex-column">
+            <ul>
+                <li>
+                    <h3>¿Cómo puedo realizar una compra en la tienda online?</h3>
+                    <p>Para realizar una compra en nuestra tienda online, simplemente tienes que elegir los productos que te interesen, añadirlos a tu carrito de compra y completar el proceso de pago.</p>
+                </li>
+                <li>
+                    <h3>¿Cuáles son las formas de pago disponibles?</h3>
+                    <p>Aceptamos tarjetas de crédito, débito, PayPal, transferencia bancaria y pago en efectivo contra reembolso.</p>
+                </li>
+                <li>
+                    <h3>¿Cuánto tardará en llegar mi pedido?</h3>
+                    <p>El tiempo de entrega depende de tu ubicación geográfica. Si te encontras en el AMBA, el plazo de entrega suele ser de 2-3 días laborables. Para otras zonas, el plazo puede variar.</p>
+                </li>
+                <li>
+                    <h3>¿Cómo puedo hacer seguimiento a mi pedido?</h3>
+                    <p>Una vez que tu pedido ha sido enviado, recibirás un correo electrónico con el número de seguimiento y un enlace para hacer seguimiento a tu pedido.</p>
+                </li>
+
+            </ul>
+        </div>
+        <div class="col-6 d-flex flex-column">
+            <ul>
+                <li>
+                    <h3>¿Qué pasa si el producto que recibo no es el que pedí?</h3>
+                    <p>Si el producto que recibes no es el que pediste, por favor ponete en contacto con nuestro equipo de atención al cliente para solucionar el problema. Te ayudaremos a encontrar una solución rápida y satisfactoria.</p>
+                </li>
+                <li>
+                    <h3>¿Puedo devolver un producto si no estoy satisfecho con él?</h3>
+                    <p>Sí, si no estás satisfecho con el producto que recibiste, puedes devolverlo en un plazo de 14 días después de haberlo recibido. Por favor, consultá nuestra política de devoluciones para más información.</p>
+                </li>
+                <li>
+                    <h3>¿Realizan envíos internacionales?</h3>
+                    <p>No, por el momento ofrecemos únicamente envíos dentro del territorio argentino.</p>
+                </li>
+                <li>
+                    <h3>¿Cuáles son los beneficios del yoga y la meditación?</h3>
+                    <p><strong>El yoga y la meditación pueden ayudarte a reducir el estrés, mejorar la salud mental, aumentar la flexibilidad, fortalecer los músculos y mejorar la calidad del sueño, entre otros beneficios. </strong></p>
+                </li>
+            </ul>
+        </div>
+
+        <p>Esperamos que estas preguntas frecuentes hayan sido útiles. Si tienes alguna otra pregunta, por favor no dudes en contactarnos. <em>Estamos aquí para ayudarte. ¡Gracias por elegir nuestra tienda online de Dhara!</em></p>
+
+    </div>
     </div>
 </section>
