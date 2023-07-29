985a9a084c690aa04b2721a2abf62d5edcb3074f
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-07-29 20:25:55 -0300

    views Panel de Usuario + Checkout

---
+++ views/checkout.php
@@ -0,0 +1,12 @@
+<div class="container">
+
+    <div class="row"></div>
+
+    <div class="col-12 p-4 mx-auto">
+        <h2>Checkout</h2>
+
+       
+
+
+    </div>
+</div>
---
+++ views/panel_usuario.php
@@ -0,0 +1,12 @@
+<div class="container">
+
+    <div class="row"></div>
+
+    <div class="col-12 p-4 mx-auto">
+        <h2>Panel de Usuario</h2>
+
+      
+
+
+    </div>
+</div>
