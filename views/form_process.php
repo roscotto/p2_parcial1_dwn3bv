c490513a3c693cadfa46bce76d9d1c01cafb9998
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-08-03 21:52:58 -0300

    creo y muestro errores de formulario contacto

--- views/form_process.php
+++ views/form_process.php
@@ -1,5 +1,7 @@
 <?PHP
 //inicializo variables con los datos que necesito traer de la superglobal
+$bandera = false;
+$mensaje = "";
 $inputNombre = $_POST['inputNombre'];
 $inputApellido = $_POST['inputApellido'];
 $inputEmail = $_POST['inputEmail'];
@@ -7,39 +9,47 @@ $inputTelefono = $_POST['inputTelefono'];
 $inputComentario = $_POST['inputComentario'];
 
 function soloNumeros($str){
-    return preg_match('/^([^0-9]*)$/', $str);
+  return preg_match('/^([^0-9]*)$/', $str);
 }
 
 function soloCaracteres($str){
-    return preg_match('/^([^a-zA-Z]*)$/', $str);
+  return preg_match('/^([^a-zA-Z]*)$/', $str);
 }
 
 function validarEmail($str){
-    return preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $str);
+  return preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $str);
 }
 
-//lo que elijo imprimir en pantalla para mostrar al usuario
-var_dump(soloNumeros($inputTelefono));
-if (true){
-    echo "<pre>";
-    print_r("<div>
-            <h2>Muchas gracias por tu mensaje!</h2>
-                <p>Te contactaremos a la brevedad.</p>
-    
-                <h3>Estos son los datos que nos enviaste:</h3>
-                <ul>
-                    <li><p>Nombre: $inputNombre </p></li>
-                    <li><p>Apellido: $inputApellido </p></li>
-                    <li><p>Email: $inputEmail </p></li>
-                    <li><p>Teléfono: $inputTelefono </p></li>
-                    <li><p>Comentario: $inputComentario </p></li>
-                </ul>
-         </div>");
-    echo "</pre>";
-}else{
-    echo "<pre>";
-    print_r("<div>
-            <h2>Tu vieja en tanga</h2>
-            ");
-    echo "</pre>";
+var_dump(soloNumeros($inputTelefono) );
+var_dump(isset($inputTelefono));
+//header('Location: ../index.php?sec=contacto');
+// die;
+if (isset($inputNombre)){
+  $mensaje = $mensaje."*el nombre es requerido <br>";
+  $bandera = true;
 }
+if (isset($inputEmail)){
+  $mensaje = $mensaje."*el email es requerido <br>";
+  $bandera = true;
+}
+if (isset($inputComentario)){
+  $mensaje = $mensaje."*el comentario es requerido <br>";
+  $bandera = true;
+}
+
+
+if ($bandera){
+  (new Alerta())->registrar_alerta('danger', $mensaje);
+  header('Location: ../index.php?sec=contacto');
+}
+?>
+<section class="contacto">
+  <div class="container mt-3  mx-auto py-5">
+    <h2>Muchas gracias por tu mensaje! <?= $inputNombre ?> <?= $inputApellido ?></h2>
+    
+    <h3>A la brebedad te estaremos contactando por la siguiente consulta:</h3>
+    <p>Comentario: <?= $inputComentario ?></p>
+  
+    <p>Te contactaremos a este email <strong><?= $inputEmail ?></strong> o a este télefono <strong><?= $inputTelefono ?></strong>.</p>
+  </div>
+</section>
